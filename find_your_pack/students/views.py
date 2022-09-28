f0934ad7069de0df1724b3aac3908c522389b974
wutever0017 <elizabethlin77@gmail.com>
2022-09-28 07:20:11 -0400

    add django app

---
+++ find_your_pack/students/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+  return HttpResponse('index')
+
+def test(request):
+  return HttpResponse("student app is up and running")
+
+def home(request):
+  return render(request, 'students/home.html')
