b371b20d1c500ed569942059bd4a5a42c8365947
wutever0017 <elizabethlin77@gmail.com>
2022-10-01 18:17:12 -0400

    static files

--- find_your_pack/find_your_pack/static/js/filter_roommate.js
+++ find_your_pack/find_your_pack/static/js/filter_roommate.js
@@ -1,1 +1,13 @@
-console.log("loaded filter_roommate");
+window.onload = (event) => {
+  console.log("loaded filter_roommate");
+}
+
+function filter_gender(){
+  console.log("checked gender checkbox");
+}
+
+function update_btn(){
+  console.log("clicked update button");
+}
+
+document.querySelectorAll("[data-mdb-filter='gender']")
